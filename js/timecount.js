239fc10b32b8b50bc172c52c18bcf48b6830769f
NiHiLine <steam2331753310@outlook.com>
2025-03-14 19:42:20 +0800

    Site updated: 2025-03-14 19:42:20

--- js/timecount.js
+++ js/timecount.js
@@ -1,12 +1,3 @@
-// 动态心跳
-$(document).ready(function(e) {
-    $('.copyright').html('©2021 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By 竹山一叶');
-})
-
-$(document).ready(function(e) {
-    show_date_time();
-})
-
 //本站运行时间
 function show_date_time() {
 $('.framework-info').html('本站已运行<span id="span_dt_dt" style="color: #fff;"></span>');
